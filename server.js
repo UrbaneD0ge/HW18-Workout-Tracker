0fcaaca044e24380259e699da8460bdc3edbd7b1
Kip <88916382+UrbaneD0ge@users.noreply.github.com>
2021-11-10 14:49:32 -0500

    server.js

--- server.js
+++ server.js
@@ -1,5 +1,28 @@
 const mongoose = require('mongoose');
 const express = require('express');
+const logger = require('morgan');
 
 const PORT = process.env.PORT || 3001;
 
+const Workout = require('./models/workout');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.use(require('./routes/apiRoutes'));
+app.use(require('./routes/htmlRoutes'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`)
+});
